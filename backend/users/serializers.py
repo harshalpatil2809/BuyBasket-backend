d28bcc94f8ea5c6a67f07999bd7f9f7ce549e62a
harshalpatil2809 <harshalpatil28095@gmail.com>
2026-04-17 22:12:41 +0530

    solving error

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,18 +1,18 @@
 from rest_framework import serializers
-from .models import User
+from .models import User  # Ensure path is correct for your 'users' app
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'name', 'password']
         extra_kwargs = {
-            'password' : {'write_only': True}
+            'password': {'write_only': True}
         }
 
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
+        # Hum direct naya UserManager use kar rahe hain
+        return User.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data.get('name', ''),
+            password=validated_data['password']
+        )
